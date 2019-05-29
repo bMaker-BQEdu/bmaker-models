52ac4c3477be9b77fa15ee4712c4afa6a9153453
Jesus Vallez <jesus.vallez@bq.com>
2019-05-29 12:42:32 +0200

    Moved license to src and create index to export schemas

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { ISBN, ISBNSchema } from './models/license/isbn/isbn.model'
+import { License, LicenseSchema } from './models/license/license/license.model'
+
+export {
+  ISBN,
+  ISBNSchema,
+  License,
+  LicenseSchema
+}
